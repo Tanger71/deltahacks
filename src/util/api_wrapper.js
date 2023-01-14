f997cbf4c6c6fd1a1878ea59ac99244f5fcb2265
sawyer <tangs50@mcmaster.ca>
2023-01-14 16:10:01 -0500

    added api func

--- src/util/api_wrapper.js
+++ src/util/api_wrapper.js
@@ -1,10 +1,24 @@
-const { Configuration, openAIApi } = require("openai");
-require('dotenv').config({path : '../.env'});
+const { Configuration, OpenAIApi } = require("openai");
+require('dotenv').config();
+
+console.log(JSON.stringify(process.env.OPEN_AI_APIKEY));
 
 const configuration = new Configuration({
    apiKey: process.env.OPEN_AI_APIKEY,
 });
 
-exports.queryChatGPT = function() {
-    console.log("Hello World!")
+const openai = new OpenAIApi(configuration);
+
+exports.queryChatGPT = async function() {
+    const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: "how do i teach someone golfing?",
+        temperature: 0.7,
+        max_tokens: 256,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+    });
+
+    return response;
 }
--- src/util/sandbox.js
+++ src/util/sandbox.js
@@ -1,3 +1,10 @@
 ///intTest
 
+var api = require('./api_wrapper');
+
+api.queryChatGPT().then(r => { 
+    console.log(r.data.choices[0].text); 
+});
+
+
 console.log("yeo");
