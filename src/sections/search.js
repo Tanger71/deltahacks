dcd409b25f363958d6f1a941cc0b3c1e8cc7c24b
OlegGlo <olegglotov99@gmail.com>
2023-01-14 17:12:50 -0500

    fixes to search.js

--- src/sections/search.js
+++ src/sections/search.js
@@ -1,19 +1,37 @@
 import React from 'react';
 import './search.css'
 
-export const SearchSection = () => {
-
-    return (
-        <section className='searchBackground'>
-            <div className='searchSection'>
-                <form>
-                    <label>
-                        I want to learn:
-                        <input type="text" name="name" />
-                    </label>
-                    <input type="submit" value="Submit" />
-                </form>
-            </div>
-        </section>
-    )
+export class SearchSection extends React.Component {
+    render() {
+        return (
+            <section className='searchBackground'>
+                <div className='searchSection'>
+                    <form>
+                        <label>
+                            I want to learn:
+                            <input type="text" name="GPTquery" />
+                        </label>
+                        <input type="submit" value="Submit" />
+                    </form>
+                </div>
+            </section>
+        )
+    }
 }
+
+// export const SearchSection = () => {
+
+//     return (
+//         <section className='searchBackground'>
+//             <div className='searchSection'>
+//                 <form>
+//                     <label>
+//                         I want to learn:
+//                         <input type="text" name="GPTquery" />
+//                     </label>
+//                     <input type="submit" value="Submit" />
+//                 </form>
+//             </div>
+//         </section>
+//     )
+// }
