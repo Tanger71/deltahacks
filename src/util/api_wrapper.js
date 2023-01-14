8788a009269c1573e5d285e7c4501950696ab0a0
r-yeh <r.yeh2008@gmail.com>
2023-01-14 16:15:30 -0500

    set up a starting point for openai integration

--- src/util/api_wrapper.js
+++ src/util/api_wrapper.js
@@ -1,10 +1,23 @@
-const { Configuration, openAIApi } = require("openai");
-require('dotenv').config({path : '../.env'});
+const { Configuration, openAIApi, OpenAIApi } = require("openai");
+require('dotenv').config();
 
 const configuration = new Configuration({
-   apiKey: process.env.OPEN_AI_APIKEY,
+    apiKey: process.env.OPEN_AI_APIKEY,
 });
+const openai = new OpenAIApi(configuration);
 
-exports.queryChatGPT = function() {
-    console.log("Hello World!")
+exports.queryChatGPT = async function() {
+    let topic = "swim";
+
+    const gptResponse = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: `How would I teach someone to ${topic}?`,
+        temperature: 0.7,
+        max_tokens: 2048,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+    })
+
+    return gptResponse["data"]["choices"][0]["text"];
 }
